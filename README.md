f14e45a5d756e4a788a0223a7e7a65e66a2d8fec
LukasWass <dev@lukaswass.se>
2021-01-20 08:43:55 +0100

    Working on simplicity and view logs web page

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 ## Log User Flow Server Azure Functions with Azure Cosmos DB
 
 Uses Azure Cosmos DB to store log data.
+
+### Usage
+
+```bash
+$ npm install --save loguf-server-azure-fuctions
+```
+
+```typescript
+import LogufServer from "loguf-server-azure-functions";
+
+const Server = new LogufServer({
+    cosmosdbUri: "my-cosmosdb-uri",
+    cosmosdbKey: "my-cosmos-db-key"
+});
+
+export default Server.createHandler();
+```
+
+### How to deploy Loguf server to azure function
+
+#### VS Code
+
+Video ...
+
+#### Azure portal
+
+Video ...
+
+### Create Azure Cosmos DB
+
+Video ...
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { CosmosClient } from "@azure/cosmos";
 interface IConstructorParam {
     cosmosdbUri: string;
     cosmosdbKey: string;
-    database: string;
-    container: string;
+    database?: string;
+    logContainer?: string;
+    userContainer?: string;
 }
 
 enum DataType {
@@ -30,44 +31,104 @@ interface ILoggedData {
 export default class LogufServerAzureFunctions {
     private cosmosdbUri: string = "";
     private cosmosdbKey: string = "";
-    private databaseName: string = "";
-    private containerName: string = "";
+    private databaseName: string = "Loguf";
+    private userContainerName: string = "Users";
+    private logContainerName: string = "Logs";
 
     constructor(params: IConstructorParam) {
         this.cosmosdbUri = params.cosmosdbUri;
         this.cosmosdbKey = params.cosmosdbKey
-        this.databaseName = params.database;
-        this.containerName = params.container;
+
+        if (params.database !== undefined)
+            this.databaseName = params.database;
+
+        if (params.userContainer !== undefined)
+            this.userContainerName = params.userContainer;
+
+        if (params.logContainer !== undefined)
+            this.logContainerName = params.logContainer;
+
+        const Client = new CosmosClient({
+            endpoint: this.cosmosdbUri,
+            key: this.cosmosdbKey
+        });
+
+        Client.databases.createIfNotExists({ id: this.databaseName, })
+            .then(() => {
+                Client.database(this.databaseName)
+                    .containers.createIfNotExists({ id: this.userContainerName, partitionKey: "/id" })
+                    .then(() => {
+                        Client.database(this.databaseName)
+                            .containers.createIfNotExists({ id: this.logContainerName, partitionKey: "/userId" })
+                            .then(() => {
+
+                            })
+                            .catch((reason) => {
+                            })
+                    })
+                    .catch((reason) => {
+                    })
+            })
+            .catch((reason) => {
+            })
+    }
+
+    private Log = (context: Context, req: HttpRequest) => {
+        const logData: IPublishData = req.body;
+
+        const data: ILoggedData = {
+            userId: logData.userId,
+            receivedTimestamp: new Date().valueOf(),
+            logData: logData,
+
+        };
+
+        const res = new CosmosClient({
+            endpoint: this.cosmosdbUri,
+            key: this.cosmosdbKey
+        })
+            .database(this.databaseName)
+            .container(this.logContainerName)
+            .items.upsert(data);
+
+
+        context.res = {
+            status: 200,
+            body: JSON.stringify({
+                success: true,
+                message: ""
+            })
+        };
+    }
+
+    private GetPortalWebsite = (context: Context, req: HttpRequest) => {
+        context.res = {
+            status: 200,
+            headers: {
+                "Content-Type": "text/html"
+            },
+            body: `
+                <html>
+                    <head>
+                        <meta http-equiv="Refresh" content="0; URL=https://www.loguf.se?server=${req.url}">
+                    </head>
+                </html>
+            `
+        };
     }
 
     public createHandler = (): AzureFunction => {
         const httpTrigger = async (context: Context, req: HttpRequest): Promise<void> => {
             try {
-                const logData: IPublishData = req.body;
-
-                const data: ILoggedData = {
-                    userId: logData.userId,
-                    receivedTimestamp: new Date().valueOf(),
-                    logData: logData,
-
-                };
-
-                const res = new CosmosClient({
-                    endpoint: this.cosmosdbUri,
-                    key: this.cosmosdbKey
-                })
-                    .database(this.databaseName)
-                    .container(this.containerName)
-                    .items.upsert(data);
-
-
-                context.res = {
-                    status: 200,
-                    body: JSON.stringify({
-                        success: true,
-                        message: ""
-                    })
-                };
+                if (req.method === "GET") {
+                    this.GetPortalWebsite(context, req);
+                }
+                else if (req.method === "POST") {
+                    this.Log(context, req);
+                }
+                else {
+                    this.Log(context, req);
+                }
             }
             catch (reason) {
                 context.res = {
